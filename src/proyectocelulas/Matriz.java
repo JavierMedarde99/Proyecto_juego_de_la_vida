02f90721fcabc9d87eb3a559cafb078cc3d1181b
javier <javiermedmata@gmail.com>
2019-04-25 21:04:04 +0200

    arreglar muerte o vida

--- src/proyectocelulas/Matriz.java
+++ src/proyectocelulas/Matriz.java
@@ -69,5 +69,59 @@ public class Matriz {
                 
             
         }
-    }    
+    }
+public   int contador(boolean a[][],int i,int j){
+            int cont1=i-1;
+            int cont2=j-1;
+            int cont=0;
+            for(int z=0;z<=8;z++){
+            if(j==-1||i==-1 ||j>=a.length||i>=a.length){
+                
+            }else{ 
+                for(int f=0;f<=3;f++){
+           for(int o=0;o<=3;o++){
+               if(a[i][j]){
+                   cont++;
+               }
+               cont2++;
+           }
+           cont1 ++;
+            }
+            
+        }
+            }
+            return cont;
+        }
+        
+public  int[][] alRededor(boolean[][]a){
+        int[][]generacion = new int[a.length][a.length];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+               generacion[i][j]=contador(a, i, j);
+            }
+        
+    } 
+return generacion;
 }
+
+public boolean[][] dios(){
+    int[][] rededor=alRededor(matrizVida);
+    for(int i=0;i<matrizVida.length;i++){
+            for(int j=0;j<matrizVida[i].length;j++){
+                if(rededor[i][j]==3 && !matrizVida[i][j]){
+                    matrizVida[i][j]=true;
+                }else if(rededor[i][j]==3 || rededor[i][j]==2 && matrizVida[i][j]){
+                    matrizVida[i][j]=true;
+                }else if(rededor[i][j]==1 || rededor[i][j]==0 && matrizVida[i][j]){
+                    matrizVida[i][j]=false;
+                }else{
+                    matrizVida[i][j]=false;
+                }
+            }
+    }
+        return matrizVida;
+    
+}
+    
+}
+
